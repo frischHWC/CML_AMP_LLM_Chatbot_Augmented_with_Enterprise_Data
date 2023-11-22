9916f4647d2e0efe172e4244bd0951bbcb028559
RISCH Francois <francois2.risch-prestataire@laposte.fr>
2023-11-22 11:10:03 +0100

    sue raw url

--- 2_job-download-models/download_models.sh
+++ 2_job-download-models/download_models.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # This script is used to pre=download files stored with git-lfs in CML Runtimes which do not have git-lfs support
 # You can use any models that can be loaded with the huggingface transformers library. See utils/model_embedding_utls.py or utils/moderl_llm_utils.py
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new"
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 EMBEDDING_MODEL_REPO="git@github.com:frischHWC/cml-amp-llm-models-repo.git"
-EMBEDDING_MODEL_BRANCH="master"
+EMBEDDING_MODEL_BRANCH="main"
 LLM_MODEL_REPO="https://huggingface.co/h2oai/h2ogpt-oig-oasst1-512-6.9b"
 LLM_MODEL_BRANCH="main"
+
+# Optional get identity of git servers (add yours here)
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan huggingface.co  >> ~/.ssh/known_hosts
+
 
 # Clear out any existing checked out models
 rm -rf ./models
@@ -21,7 +26,7 @@ echo "Finished download of LFS file for embedding-model"
 cd ..
   
 # Downloading LLM model that has been fine tuned to handle instructions/q&a
-GIT_LFS_SKIP_SMUDGE=1 git clone ${LLM_MODEL_REPO} --branch ${EMBEDDING_MODEL_BRANCH} llm-model
+GIT_LFS_SKIP_SMUDGE=1 git clone ${LLM_MODEL_REPO} --branch ${LLM_MODEL_BRANCH} llm-model
 cd llm-model
 # You must provide ALL urls of .lfs files (examples are provided below)
 curl -O -L "https://raw.githubusercontent.com/frischHWC/cml-amp-llm-models-repo/master/pymodel.bin"
